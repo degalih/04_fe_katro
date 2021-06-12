7d89d1fe4047d37d193246c068549fd3c32d60d8
Galih Muhammad Ramdan <galihmramdan17@gmail.com>
2021-06-12 16:40:37 +0700

    04 - Revisi 4 	* Merubah kontras warna setelah analisis lighthouse. 	* Mengubah konstruksi styles website. 	* Menghapus sass file.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
 import "regenerator-runtime"; /* for async await transpile */
 import "../styles/css/main.css";
+import "../styles/css/responsive.css";
+import "../styles/css/root.css";
 import "./mobile-menu";
