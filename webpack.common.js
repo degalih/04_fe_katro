73dba2b104639dc170d67672dd70e7e509de8457
Galih Muhammad Ramdan <galihmramdan17@gmail.com>
2021-10-04 20:50:35 +0700

    comment ignore

--- webpack.common.js
+++ webpack.common.js
@@ -42,9 +42,9 @@ module.exports = {
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
-          globOptions: {
-            ignore: ['**/images/**'], // CopyWebpackPlugin mengabaikan berkas yang berada di dalam folder images
-          },
+          /*           globOptions: {
+            ignore: ['**/ images /**'], // CopyWebpackPlugin mengabaikan berkas yang berada di dalam folder images
+          }, */,
         },
       ],
     }),
